86aa6bc58311f73f81f085818fea14afcb2cc241
josejesus3 <gutierresrayas88@gmail.com>
2026-05-07 01:15:37 -0600

    Creacion de estructura/Carpetas/archivos

---
+++ src/app/features/dashboard/components/chart-summary/chart-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-summary.component.html',
+  styleUrl: './chart-summary.component.scss'
+})
+export class ChartSummaryComponent {
+
+}
