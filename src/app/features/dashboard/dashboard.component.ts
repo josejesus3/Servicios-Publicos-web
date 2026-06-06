6f763646c68cf9218857f7f852856dd48600708e
Jose de Jesus Gutierrez Rayas <josegutierres@MacBook-Pro-de-Jose.local>
2026-06-05 22:11:38 -0600

    login y chart-summary

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CategoryComponentComponent } from "./components/category-component/category-component.component";
 import { HeaderComponentComponent } from "./components/header-component/header-component.component";
+import { ChartSummaryComponent } from "./components/chart-summary/chart-summary.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CategoryComponentComponent, HeaderComponentComponent],
+  imports: [CategoryComponentComponent, HeaderComponentComponent, ChartSummaryComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
