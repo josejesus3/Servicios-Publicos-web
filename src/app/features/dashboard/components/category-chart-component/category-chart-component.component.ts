59d5c577821be148c36491c7a4940778072c4417
Jose de Jesus Gutierrez Rayas <josegutierres@MacBook-Pro-de-Jose.local>
2026-06-12 03:03:06 -0600

    nuevo diseño

--- src/app/features/dashboard/components/category-chart-component/category-chart-component.component.ts
+++ src/app/features/dashboard/components/category-chart-component/category-chart-component.component.ts
@@ -1,19 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-category-chart-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,MatCardModule, MatButtonModule],
   templateUrl: './category-chart-component.component.html',
   styleUrl: './category-chart-component.component.scss'
 })
-export class CategoryChartComponentComponent {
-  areas=[
-    {area:"Areas verdes",reports:128},
-    {area:"Alumbrado Publico",reports:100},
-    {area:"Limpieza",reports:90},
-    {area:"Baches",reports:40},
-    {area:"Alcantarillado",reports:90}
-  ];
+export class CategoryChartComponentComponent implements OnInit {
+  areas:any[]=[];
+
+  
+  ngOnInit(): void {
+    this.getAreas();
+  }
+  private apiUrl= environment.ApiUrl;
+  private http=inject(HttpClient);
+
+  getAreas(){
+    this.http.get<any>(`${this.apiUrl}/areas`).subscribe({
+      next:(response)=>{
+      this.areas=response.data;
+      }
+    });
+
+  }
+
+  getIconos(categorias:string){
+   const iconos: any = {
+  'Limpieza': 'bi bi-trash-fill text-info fs-2',
+  'Bacheo': 'bi bi-signpost-fill text-warning fs-2',
+  'Alumbrado Público': 'bi bi-lightbulb-fill text-warning fs-2',
+  'Fuga de Agua': 'bi bi-droplet-fill text-primary fs-2',
+  'Áreas Verdes': 'bi bi-tree-fill text-success fs-2',
+  'Agua Potable': 'bi bi-droplet-half text-primary fs-2',
+  'Aseo Público': 'bi bi-trash3-fill text-info fs-2',
+  'Parques y Jardines': 'bi bi-tree-fill text-success fs-2'
+};
+
+  return iconos[categorias]|| 'fa-solid fa-circle-info';
+  }
+
+
+  
+
 
 }
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CategoryComponentComponent } from "./components/category-component/category-component.component";
 import { HeaderComponentComponent } from "./components/header-component/header-component.component";
 import { ChartSummaryComponent } from "./components/chart-summary/chart-summary.component";
+import { LoginComponent } from "../auth/login/login.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CategoryComponentComponent, HeaderComponentComponent, ChartSummaryComponent],
+  imports: [CategoryComponentComponent, HeaderComponentComponent, ChartSummaryComponent, LoginComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
